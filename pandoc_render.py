884d72dba9612b15ec8447d10774aae0ad6fbb40
Muchenxuan Tong <demon386@gmail.com>
2012-07-02 16:14:00 +0800

    Added header comment; changed some stylical errors (with SublimeLinter)

--- pandoc_render.py
+++ pandoc_render.py
@@ -1,20 +1,32 @@
-import sublime, sublime_plugin
+"""This file is initially forked from
+[SublimePandoc](https://github.com/jclement/SublimePandoc)
+by [DanielMe](https://github.com/DanielMe/)
+
+@todo naming convention should be foo_bar rather than fooBar.
+@bug PDF export doesn't work in my Mac, gonna check it later.
+
+2012-07-02: Muchenxuan Tong changed some stylical errors (with SublimeLinter)
+"""
+
+import sublime
+import sublime_plugin
 import webbrowser
 import tempfile
 import os
 import sys
 import subprocess
 
-class PandocRenderCommand(sublime_plugin.TextCommand):
 
+class PandocRenderCommand(sublime_plugin.TextCommand):
     def is_enabled(self):
         return self.view.score_selector(0, "text.html.markdown") > 0
 
     def is_visible(self):
-        return True 
+        return True
 
     def run(self, edit, target="pdf", openAfter=True, saveResult=False, additionalArguments=[]):
-        if not target in ["html","docx","pdf"]: raise Exception("Format %s currently unsopported" % target)
+        if target not in ["html", "docx", "pdf"]:
+            raise Exception("Format %s currently unsopported" % target)
 
         encoding = self.view.encoding()
         if encoding == 'Undefined':
@@ -32,8 +44,8 @@ class PandocRenderCommand(sublime_plugin.TextCommand):
         # output file...
         suffix = "." + target
         if saveResult:
-            output_name = os.path.splitext(self.view.file_name())[0]+suffix
-            if not self.view.file_name(): 
+            output_name = os.path.splitext(self.view.file_name())[0] + suffix
+            if not self.view.file_name():
                 raise Exception("Please safe the buffer before trying to export with pandoc.")
         else:
             output = tempfile.NamedTemporaryFile(delete=False, suffix=suffix)
@@ -41,19 +53,19 @@ class PandocRenderCommand(sublime_plugin.TextCommand):
             output_name = output.name
 
         args = self.pandoc_args(target)
-        self.run_pandoc(tmp_md.name,output_name,args)
-        if openAfter: self.open_result(output_name,target)
+        self.run_pandoc(tmp_md.name, output_name, args)
+        if openAfter:
+            self.open_result(output_name, target)
 
-    def run_pandoc(self, infile, outfile,args):
-        cmd  = ['pandoc'] + args
+    def run_pandoc(self, infile, outfile, args):
+        cmd = ['pandoc'] + args
         cmd += [infile, "-o", outfile]
         try:
             subprocess.call(cmd)
         except Exception as e:
             sublime.error_message("Unable to execute Pandoc.  \n\nDetails: {0}".format(e))
 
-
-    def pandoc_args(self,target):
+    def pandoc_args(self, target):
         """
         Create a list of arguments for the pandoc command
         depending on the target.
@@ -66,7 +78,7 @@ class PandocRenderCommand(sublime_plugin.TextCommand):
         if target == "docx":
             return ['-t', 'docx']
 
-    def open_result(self,outfile,target):
+    def open_result(self, outfile, target):
         if target == "html":
             webbrowser.open_new_tab(outfile)
         elif sys.platform == "win32":
